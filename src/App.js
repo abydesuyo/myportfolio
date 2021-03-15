60754dd3b341cc305ac10b3d9282fd1fd44cae03
abydesuyo <38089225+abydesuyo@users.noreply.github.com>
2021-03-15 09:24:32 +0900

    Added missing files to repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+// import './style.css';
+// import './flexslider.css';
+import './css/style.css';
+import './css/flexslider.css';
+import './css/bootstrap.css';
+import './css/animate.css';
+// import Header from './Components/Header';
+// import Footer from './Components/Footer';
+import Introduction from './Components/Introduction';
+import About from './Components/About';
+// import Resume from './Components/Resume';
+// import Contact from './Components/Contact';
+// import Testimonials from './Components/Testimonials';
+// import Portfolio from './Components/Portfolio';
+import Sidebar from './Components/Sidebar';
+import $ from 'jquery';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      foo: 'bar',
+      websiteData: {}
+    };
+
+  }
+
+  getSiteData(){
+    $.ajax({
+      url:'websiteData.json',
+      dataType:'json',
+      cache: false,
+      success: function(data){
+        this.setState({websiteData: data});
+      }.bind(this),
+      error: function(xhr, status, err){
+        console.log(err);
+        alert(err);
+      }
+    });
+  }
+
+  componentDidMount(){
+    this.getSiteData();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div id="colorlib-page">
+            <div id="container-wrap">
+              <Sidebar data={this.state.websiteData.main}/>
+              <div id="colorlib-main">
+                <Introduction data={this.state.websiteData.main}/> 
+                <About data={this.state.websiteData.main}/>
+              </div>
+            </div>
+          </div>
+      </div>
+    );
+  }
 }
 
 export default App;
